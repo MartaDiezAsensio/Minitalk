7d5400899b26ca13d11816b1d225a27a50e2fe34
Marta Diez Asensio <mdiez-as@car12s1.42barcelona.com>
2023-06-13 17:41:06 +0200

    Write instead of printf in sighandler

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 18:58:37 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/06/08 16:19:28 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:38:16 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	sig_handler(int sig)
 {	
 	static int	bit = 0;
 	static int	c = 0;
+	char		chr;
 
 	if (sig == SIGUSR1)
 		c = c | (0x01 << bit);
 	if (++bit == 8)
 	{
-		printf("%c", c);
+		chr = c;
+		write(1, &chr, 1);
 		bit = 0;
 		c = 0;
 	}
@@ -40,15 +42,18 @@ int	main(int argc, char **argv)
 	}
 	pid = getpid();
 	printf("PID = %d\n", pid);
+
 	sa.sa_handler = sig_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
+
 	if (sigaction(SIGUSR1, &sa, NULL) == -1
 		|| sigaction(SIGUSR2, &sa, NULL) == -1)
 	{
 		printf("Error");
 		return (1);
 	}
+	
 	while (1)
 		sleep(1);
 	return (0);
